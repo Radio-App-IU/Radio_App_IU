417640610aa8f1d347207c4a172e1c59f0184a35
fege <f.ege@web.de>
2022-03-06 18:07:53 +0100

    3nd Design commited

--- app/src/main/java/com/example/radio_app_iu/MainActivity.kt
+++ app/src/main/java/com/example/radio_app_iu/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.radio_app_iu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+  //      var buttonBewerten : Button = findViewById(R.id.buttonbewerten);
+  //      buttonBewerten.setBackgroundColor(0x654688);
     }
 }
