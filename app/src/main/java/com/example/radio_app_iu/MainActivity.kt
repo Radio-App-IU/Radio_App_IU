2d4fea6306200875b8fc96238381fd5d9eba204e
Timo <timbowiebe1996@googlemail.com>
2022-03-07 17:57:14 +0100

    First Version

--- app/src/main/java/com/example/radio_app_iu/MainActivity.kt
+++ app/src/main/java/com/example/radio_app_iu/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.radio_app_iu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.radio_app_iu.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
