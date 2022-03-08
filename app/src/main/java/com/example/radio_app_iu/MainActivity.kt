e434bcde56f88e14b470528c55f0173eab6206f9
fege <f.ege@web.de>
2022-03-08 14:50:21 +0100

    Binding commited

--- app/src/main/java/com/example/radio_app_iu/MainActivity.kt
+++ app/src/main/java/com/example/radio_app_iu/MainActivity.kt
@@ -5,19 +5,21 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import com.example.radio_app_iu.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main) unnötig durch Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         //      var buttonBewerten : Button = findViewById(R.id.buttonbewerten);
         //      buttonBewerten.setBackgroundColor(0x654688);
 
-        //Instanziierien des Playbuttons
-        val playbutton : Button = findViewById(R.id.playbuttoninvisible)
-
-        //Playbutton OmClickListener
-        playbutton.setOnClickListener {
+        //OnClickListener Playbutton
+        binding.playbuttoninvisible.setOnClickListener {
                 val v: ImageView = findViewById(R.id.playbutton)
                     v.setImageResource(R.drawable.playbutton1)
                 }
