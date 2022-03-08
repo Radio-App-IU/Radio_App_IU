489ab86527ec1e660b9040f73a63d06e3b864a8e
fege <f.ege@web.de>
2022-03-08 15:28:21 +0100

    Playbutton ein/aus

--- app/src/main/java/com/example/radio_app_iu/MainActivity.kt
+++ app/src/main/java/com/example/radio_app_iu/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.radio_app_iu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
 import com.example.radio_app_iu.databinding.ActivityMainBinding
 
@@ -12,16 +11,20 @@ private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main) unnötig durch Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //      var buttonBewerten : Button = findViewById(R.id.buttonbewerten);
         //      buttonBewerten.setBackgroundColor(0x654688);
 
-        //OnClickListener Playbutton
         binding.playbuttoninvisible.setOnClickListener {
-                val v: ImageView = findViewById(R.id.playbutton)
-                    v.setImageResource(R.drawable.playbutton1)
+            binding.playbutton.setImageResource(R.drawable.playbutton1)
+            binding.playbuttoninvisible.visibility = View.INVISIBLE
+            binding.playbuttoninvisible2.visibility = View.VISIBLE
                 }
+        binding.playbuttoninvisible2.setOnClickListener {
+            binding.playbutton.setImageResource(R.drawable.playbutton2)
+            binding.playbuttoninvisible2.visibility = View.INVISIBLE
+            binding.playbuttoninvisible.visibility = View.VISIBLE
         }
+    }
 }
